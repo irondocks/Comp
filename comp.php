ac561ca20edd54f3fcbbd81d83dd5bb90fccf119
wise-penny <tcp@null.net>
2020-12-02 13:51:10 -0500

    Update comp.php

--- comp.php
+++ comp.php
@@ -4,21 +4,24 @@ function Compress(string $file, string $outputname)
 {
     $m = 0;
     $i = 25;
-    $s = 6400;
+    $s = 10000;
     while (strlen($file)%$s > 0) $s--;
     $img = imagecreatetruecolor((($s)),round(strlen($file)/$s)+5);
     $black = imagecolorallocate($img, 1, 1, 1 );
     imagefill($img, 0, 0, $black);
     $rr = 0;
     $y = 0;
-    imageantialias($img, false);
+    //imageantialias($img, false);
     
     while (strlen($file) > $m)
     {
         $r = ord($file[$m]);
         $black = ($r << 16) + ($r << 8) + $r;
         $m += 1;
-
+        // $r = ord($file[$m]);
+        // $black = ($r << 8) + $black;
+        // $m += 1;
+ 
         imagesetpixel($img,$rr,$y,$black);
         $rr++;
         
@@ -27,44 +30,15 @@ function Compress(string $file, string $outputname)
             $y += 1;
             $rr = 0;
         }
-        if ($m < strlen($file))
-        {
-            $r = ord($file[$m]);
-            $black = ($r << 16) + ($r << 8) + $r;
-            $m += 1;
-
-            imagesetpixel($img,$rr,$y,$black);
-            $rr++;
-        }
-        if ($rr+1 >= (($s)))
-        {
-            $y += 1;
-            $rr = 0;
-        }
-        if ($m < strlen($file))
-        {
-            $r = ord($file[$m]);
-            $black = ($r << 16) + ($r << 8) + $r;
-            $m += 1;
-
-            imagesetpixel($img,$rr,$y,$black);
-            $rr++;
-        }
-        if ($rr+1 >= (($s)))
-        {
-            $y += 1;
-            $rr = 0;
-        }
     }
     $int = (($s));
     $img = imagecrop($img,["x" => 0, "y" => 0, "width" => $int, "height" => ($y+1)]);
-    //imagefilter($img,IMG_FILTER_GRAYSCALE);
-    imagepng($img, "$outputname.png",9);
-
+    imageantialias($img, false);
+    imagepng($img, "$outputname.png", 9);
     echo "Compressed....";
-    //exec("gm convert -define jpeg:optimize-coding=true -define webp:lossless=true -colors 256 -colorspace GRAY $outputname.png $outputname.webp");
-    exec("cwebp -lossless -noalpha -q 100 $outputname.png -o $outputname.webp");
-    unlink("$outputname.png");
+    $dim = $s . "x" . ($y+1) . "+256";
+    exec("gm convert -colorspace gray -size $dim -depth 8 -colors 256 -define webp:lossless=true $outputname.png $outputname.webp");
+    //unlink("$outputname.png");
     imagedestroy($img);
 }
 
@@ -80,18 +54,14 @@ function Decompress(string $filename, string $output = "f.txt")
     while (imagesy($img) > $y)
     {
 
-        $rgb = imagecolorat($img,$rr,$y);
-
-        $b = chr($rgb & 0xFF);
-
-        $rr++;
-        
-        if ($rr+1 >= imagesx($img))  
-        {
-            $y += 1;
-            $rr = 0;
+        while (imagesx($img) > $rr + 1) {
+            $rgb = imagecolorat($img,$rr,$y);
+            $b = chr($rgb & 0xFF);
+            $str .= $b;
+            $rr++;
         }
-        $str .= $b;
+        $rr = 0;
+        $y++;
     }
     //rtrim($str,"\x00");
     while (substr($str,-1) == chr(1))
@@ -107,15 +77,17 @@ $c = "";
 while (strlen($filestr) > 0)
 {
     $c = chr($x); 
-    if (strlen($filestr) > 500000000)
+    if (strlen($filestr) > 50000000)
     {
-        Compress(substr($filestr,0,500000000), "$d$c");
-        $filestr = substr($filestr,500000000);
+        Compress(substr($filestr,0,50000000), "$d$c");
+        $filestr = substr($filestr,50000000);
+        //Decompress("$d$c.webp","f.txt");
     }
     else
     {
         Compress($filestr, "$d$c");
         $filestr = "";
+        Decompress("$d$c.webp","f.txt");
     }
     if (chr($x) == 'Z')
     {
@@ -124,7 +96,6 @@ while (strlen($filestr) > 0)
     }
     $x++;
 }
-Decompress("$d$c.webp","f.txt");
 
 echo "done;";
 ?>
